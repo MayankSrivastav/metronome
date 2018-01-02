3ec48623a2e2ab0bcb66b0720d584132f0a1130d
Mayank Srivastav <mayanksri18@yahoo.in>
2018-01-02 21:35:34 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Metronome from './Metronome';
+import registerServiceWorker from './registerServiceWorker';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
