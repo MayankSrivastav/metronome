9a5cf1a4c7944736c817fc3154e6ceb3f9834c8b
Mayank Srivastav <mayanksri18@yahoo.in>
2018-01-03 21:02:41 +0530

    Added a Metronome component

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ import './index.css';
 import Metronome from './Metronome';
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+class Metronome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bpm: 100,
+      count: 0,
+      playing: false,
+      beatsPerMeasure: 4
+    };
+
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
+}
